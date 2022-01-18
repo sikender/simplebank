e45d15f3d1249fe539d792eca9eab4692685fe3c
Sikender Khan <sikendermkhan@gmail.com>
2022-01-18 11:54:31 +0530

    Update Unit Tests for Accounts

--- db/query/account.sql
+++ db/query/account.sql
@@ -5,7 +5,7 @@ INSERT INTO accounts (owner, balance, currency) VALUES ($1, $2, $3) RETURNING *;
 SELECT * FROM accounts WHERE id = $1 LIMIT 1;
 
 -- name: ListAccounts :many
-SELECT * FROM accounts ORDER BY owner LIMIT $1 OFFSET $2;
+SELECT * FROM accounts ORDER BY id LIMIT $1 OFFSET $2;
 
 -- name: UpdateAccount :one
 UPDATE accounts SET balance = $2 WHERE id = $1 RETURNING *;
--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -57,7 +57,7 @@ func (q *Queries) GetAccount(ctx context.Context, id int64) (Account, error) {
 }
 
 const listAccounts = `-- name: ListAccounts :many
-SELECT id, owner, balance, currency, created_at FROM accounts ORDER BY owner LIMIT $1 OFFSET $2
+SELECT id, owner, balance, currency, created_at FROM accounts ORDER BY id LIMIT $1 OFFSET $2
 `
 
 type ListAccountsParams struct {
