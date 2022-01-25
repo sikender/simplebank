47b3502cf2508c3717055658a1204b3fc5390e2e
Sikender Khan <sikendermkhan@gmail.com>
2022-01-25 23:14:13 +0530

    Add api routes and config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -8,11 +8,7 @@ import (
 
 	"github.com/jaswdr/faker"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/sikender/simplebank/util"
 )
 
 var random = faker.New()
@@ -20,8 +16,12 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
